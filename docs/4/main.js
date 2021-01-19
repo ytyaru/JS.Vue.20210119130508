5f661fadc4fe8afe2e2f9b9fd0d90d0a356baf9b
ytyaru <yttry0@gmail.com>
2021-01-19 15:15:30 +0900

    v-on

---
+++ docs/4/main.js
@@ -0,0 +1,14 @@
+var app;
+window.addEventListener('load', (event) => {
+    app = new Vue({
+        el: '#app',
+        data: {
+            message: 'Hello Vue.js !!'
+        },
+        methods: {
+            reverseMessage: function() {
+                this.message = this.message.split('').reverse().join('');
+            }
+        }
+    })
+});
