805df0d547be7357e8809873fc6308c8a6047939
ytyaru <yttry0@gmail.com>
2021-01-19 14:21:47 +0900

    v-bind

---
+++ docs/1/main.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', (event) => {
+    new Vue({
+        el: '#app',
+        data() {
+            return {
+                text: "Hello Vue.js !!"
+            };
+        }
+    })
+});
