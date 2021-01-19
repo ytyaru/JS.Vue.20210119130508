ebd41058666d3d6b887292ee796507767d930cce
ytyaru <yttry0@gmail.com>
2021-01-19 13:20:53 +0900

    clean

--- docs/0/main.js
+++ docs/0/main.js
@@ -1,9 +1,4 @@
 window.addEventListener('load', (event) => {
-    /*
-    const msg = 'Hello JavaScript !!'
-    console.log(msg);
-    alert(msg);
-    */
     new Vue({
         el:'#app',
         data(){
